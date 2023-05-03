b6871f5241603ff8bc8283d4e3a8daccc70f1ae4
rishi <rishi.patel@dignizant.com>
2023-05-03 15:56:39 +0530

    resume Update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Rishi_MERN_Stack_Dev_Resume.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import React, { useState, useEffect } from "react"
+import { Container, Row } from "react-bootstrap"
+import Button from "react-bootstrap/Button"
+import Particle from "../Particle"
+import pdf from "../../Assets/../Assets/Rishi_Full-stack_Dev_Resume.pdf"
+import { AiOutlineDownload } from "react-icons/ai"
+import { Document, Page, pdfjs } from "react-pdf"
+import "react-pdf/dist/esm/Page/AnnotationLayer.css"
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
-const resumeLink = pdf;
+const resumeLink = pdf
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+  const [width, setWidth] = useState(1200)
 
   useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
+    setWidth(window.innerWidth)
+  }, [])
 
   return (
     <div>
@@ -52,7 +52,7 @@ function ResumeNew() {
         </Row>
       </Container>
     </div>
-  );
+  )
 }
 
-export default ResumeNew;
+export default ResumeNew
